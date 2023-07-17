23549f82223dc4fdb6c7ca8ba8ec0f8ca0b90de1
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-17 18:24:39 +0100

    Wrote function that plays one round, does not take user input correctly yet

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 /* JS script for Rock paper scissors game */
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    if (choice === 0) {
-        choice = 'rock';
-        } else if (choice === 1) {
-            choice = 'paper';
-        } else if (choice === 2) {
-            choice = 'scissors';
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        computerChoice= 'rock';
+        } else if (computerChoice === 1) {
+            computerChoice = 'paper';
+        } else if (computerChoice === 2) {
+            computerChoice = 'scissors';
         }
-    return choice;
+    return computerChoice;
 }
 
-getComputerChoice();
+function playGame(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {
+        alert('It\'s a draw');
+    } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+        alert("You win!, Scissors beats Rock");
+    } else if (playerChoice == 'paper' && computerChoice == 'rock') {
+        alert('You win!, Paper beats Rock');
+    } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+        alert('You win!, Rock beats Scissors');
+    } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        alert('You lose!, Rock beats Scissors');
+    } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        alert('You lose!, Scissors beats Paper');
+    } else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        alert('You lose!, Paper beats Rock');
+    }
+    console.log(computerChoice);
+    console.log(playerChoice);
+}
