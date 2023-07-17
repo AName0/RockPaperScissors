bdedea582500e5e9233e58255920192a38f13ac2
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-17 21:00:16 +0100

    Add game() function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* JS script for Rock paper scissors game */
+function getPlayerChoice() {
+    let playerChoice = prompt("Rock, Paper or Scissors?")
+    return playerChoice
+}
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
@@ -12,22 +16,32 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function playGame(playerChoice, computerChoice) {
-    if (playerChoice == computerChoice) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
         alert('It\'s a draw');
-    } else if (playerChoice == 'scissors' && computerChoice == 'paper') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         alert("You win!, Scissors beats Rock");
-    } else if (playerChoice == 'paper' && computerChoice == 'rock') {
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         alert('You win!, Paper beats Rock');
-    } else if (playerChoice == 'rock' && computerChoice == 'scissors') {
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         alert('You win!, Rock beats Scissors');
-    } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         alert('You lose!, Rock beats Scissors');
-    } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         alert('You lose!, Scissors beats Paper');
-    } else if (playerChoice == 'rock' && computerChoice == 'paper') {
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         alert('You lose!, Paper beats Rock');
     }
-    console.log(computerChoice);
-    console.log(playerChoice);
+    console.log(playerSelection);
+    console.log(computerSelection);
 }
+
+function game() {
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+    playRound(getPlayerChoice(), getComputerChoice());
+}
+
+game();
