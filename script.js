08416baf0bb2944503d3e74a913779d43f2b2f6d
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-20 00:46:07 +0100

    All counters now update

--- script.js
+++ script.js
@@ -27,30 +27,37 @@ function playRound(playerSelection, computerSelection) {
         alert('It\'s a draw');
         console.log('draw');
         drawCounter += 1;
+        drawCounterDisplay.textContent = `Draws: ${drawCounter}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         alert("You win!, Scissors beats Rock");
         console.log('player wins')
         winCounter += 1;
+        winCounterDisplay.textContent = `Wins: ${winCounter}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         alert('You win!, Paper beats Rock');
         console.log('player wins')
         winCounter += 1;
+        winCounterDisplay.textContent = `Wins: ${winCounter}`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         alert('You win!, Rock beats Scissors');
         console.log('player wins')
         winCounter += 1;
+        winCounterDisplay.textContent = `Wins: ${winCounter}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         alert('You lose!, Rock beats Scissors');
         console.log('player loses')
         lossCounter += 1;
+        lossCounterDisplay.textContent = `Losses: ${lossCounter}`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         alert('You lose!, Scissors beats Paper');
         console.log('player loses')
         lossCounter += 1;
+        lossCounterDisplay.textContent = `Losses: ${lossCounter}`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         alert('You lose!, Paper beats Rock');
         console.log('player loses')
         lossCounter += 1;
+        lossCounterDisplay.textContent = `Losses: ${lossCounter}`;
     }
 }
 
