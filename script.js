0d5b1e3abb09ed3a7fcff45ca6329c72f3dcb823
AName0 <115248908+AName0@users.noreply.github.com>
2023-07-20 19:36:22 +0100

    added semi working restart button

--- script.js
+++ script.js
@@ -18,41 +18,43 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         alert('It\'s a draw');
-        console.log('draw');
         drawCounter += 1;
         drawCounterDisplay.textContent = `Draws: ${drawCounter}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         alert("You win!, Scissors beats Rock");
-        console.log('player wins')
         playerScore += 1;
         playerScoreDisplay.textContent = `Player: ${playerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         alert('You win!, Paper beats Rock');
-        console.log('player wins')
         playerScore += 1;
         playerScoreDisplay.textContent = `Player: ${playerScore}`;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         alert('You win!, Rock beats Scissors');
-        console.log('player wins')
         playerScore += 1;
         playerScoreDisplay.textContent = `Player: ${playerScore}`;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         alert('You lose!, Rock beats Scissors');
-        console.log('player loses')
         computerScore += 1;
         computerScoreDisplay.textContent = `Computer: ${computerScore}`;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         alert('You lose!, Scissors beats Paper');
-        console.log('player loses')
         computerScore += 1;
         computerScoreDisplay.textContent = `Computer: ${computerScore}`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         alert('You lose!, Paper beats Rock');
-        console.log('player loses')
         computerScore += 1;
         computerScoreDisplay.textContent = `Computer: ${computerScore}`;
     }
     checkScore();
+}
+
+function restartGame() {
+    playerScore = 0;
+    computerScore = 0;
+    drawCounter = 0;
+    playerScoreDisplay.textContent = `Player: ${playerScore}`;
+    computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+    drawCounterDisplay.textContent = `Draws: ${drawCounter}`;
 }
 
 function checkScore() {
@@ -60,6 +62,10 @@ function checkScore() {
         const finalResult = document.createElement('p');
         finalResult.textContent = 'Player Wins!';
         container.appendChild(finalResult);
+        const restartButton = document.createElement('button'); //TODO: prevent player from clicking rps buttons so only oen restart button can appear
+        restartButton.textContent = 'Restart';                  //TODO: allow restart button to be removed when pressed and also update text on screen.
+        restartButton.addEventListener('click', () => restartGame())
+        container.appendChild(restartButton);
     } else if (computerScore === 5) {
         const finalResult = document.createElement('p');
         finalResult.textContent = 'Computer Wins!';
